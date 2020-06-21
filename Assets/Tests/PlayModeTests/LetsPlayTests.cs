508acf0c59a0856d137b9e49db67b4253a43ddf9
Deepak Chirammel <dchirammel@lenovo.com>
2020-06-21 23:51:43 +0530

    Lets Play - Play Mode Tests

---
+++ Assets/Tests/PlayModeTests/LetsPlayTests.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.TestTools;
+using UnityEngine.UI;
+
+namespace Tests
+{
+    public class LetsPlayTests
+    {
+        [OneTimeSetUp]
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("LetsPlay");
+        }
+
+        [UnityTest]
+        public IEnumerator ShouldCalculateSumInstantly([Values(12, 97)] int a, [Values(13, 23)] int b)
+        {
+            GameObject.Find("InputFieldA").GetComponent<InputField>().text = a.ToString();
+            GameObject.Find("InputFieldB").GetComponent<InputField>().text = b.ToString();
+            yield return null;
+            Assert.AreEqual((a + b).ToString(), GameObject.Find("SumText").GetComponent<Text>().text);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # LetsPlay
+https://medium.com/@deepak.chirammel/practical-playmode-testing-in-unity3d-5ea455bf28b0
